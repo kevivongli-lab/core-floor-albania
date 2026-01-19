ed37a0be94df1f345a9e288e4bbd3cd33e902af9
Wix Remote Machine <support@wix.com>
2026-01-19 17:27:32 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -180,7 +180,7 @@ export default function HomePage() {
                 <Image 
                   src="https://static.wixstatic.com/media/b9443e_1f2e64823dd0484199056f6417815d18~mv2.png" 
                   alt="FirmFit Logo" 
-                  width={500}
+                  width={700}
                   className="h-auto max-w-full"
                 />
               </div>
